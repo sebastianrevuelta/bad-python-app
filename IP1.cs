e1fae58ceea50dc90d6ca62fc37bf0bea245ef29
Sebastian Revuelta <sebastianrevuelta@gmail.com>
2023-09-13 11:03:29 +0200

    Update IP1.cs

--- IP1.cs
+++ IP1.cs
@@ -4,7 +4,7 @@ using System.Net;
 public class IPAddressExample
 {
     private IPAddress hardcodedIpAddress;
-    public const string MyIPAddress = "123.168.96.53";
+    public const string MyIPAddress = "123.168.96.52";
 
     public IPAddressExample()
     {
